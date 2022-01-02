2a3b127899191b177d849d4ddca5ca420f90ac19
Logravia <j.alunans@gmail.com>
2022-01-02 18:01:43 +0200

    add clearAll func

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 // holds number input by user as seperate chars
-input = [[],[]]
+// adds pressed number to input1 or input2 according to calculator logic
+const processNumInput = (e) => {
+
+  let btnNum = e.currentTarget.textContent;
+
+  if (input[curInput].length < screenNumLim) {
+    input[curInput].push(btnNum);
+  }
+
+  updateScreen();
+}
+const updateScreen = () => {
+  screen.textContent = input[curInput].join('');
+}
+const processFuncInput = (e) => {}
+const clearAll = function onACPressResetEverything() {
+    input = [[],[]]
+    curInput = 0;
+    operand = "";
+    calcMem = 0;
+    updateScreen();
+}
+const shrinkRow = function onButtonClickShrinkButtonRow () {
+
+}
+
+let input = [[],[]]
 
 // tracks which in
 let curInput = 0;
@@ -12,26 +38,8 @@ let calcMem = 0;
 const screenNumLim = 10;
 const screen = document.querySelector('#screen')
 
-
-// adds pressed number to input1 or input2 according to calculator logic
-const processNumInput = (e) => {
-  let btnNum = e.currentTarget.textContent;
-
-  if (input[curInput].length < screenNumLim) {
-    input[curInput].push(btnNum);
-  }
-  updateScreen();
-}
-
-const updateScreen = () => {
-  screen.textContent = input[curInput].join('');
-}
-
-
-
 const numPad = document.querySelectorAll('.num-btn');
 numPad.forEach(numBtn => numBtn.addEventListener('click', processNumInput))
 
-const shrinkRow = function onButtonClickShrinkButtonRow () {
-
-}
+const acBtn = document.querySelector("#ac")
+acBtn.addEventListener('click', clearAll);
