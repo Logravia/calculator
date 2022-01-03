108ccbc05089183d3acb9354f22090f6a928c80b
Logravia <j.alunans@gmail.com>
2022-01-03 21:13:31 +0200

    fix bugs by refactoring and updating logic

--- script.js
+++ script.js
@@ -1,68 +1,79 @@
-// holds number input by user as seperate chars
-// adds pressed number to input1 or input2 according to calculator logic
 const processNumpadInput = (e) => {
+
+  // when user has defined operation it means it's time to write into second register
+  if (operation !== undefined) {
+    curReg = 1;
+  }
 
   let btnValue = e.currentTarget.textContent;
 
-  if (input[curInput].length === 0 && (btnValue === '.' )){
+  // when inputting zero when only zero is registered, do nothing except update the screen
+  if (inputRegister[curReg].length === 1 && btnValue === '0' && inputRegister[curReg][0] === 0) {
+    updateScreen();
     return;
   }
 
-  if (input[curInput].length === 1 && btnValue === '0') {
-    return;
-  }
-
-
-
-  
-
+  // allow to set only one decimal point
   if (btnValue === '.') {
-    if (isFloat[curInput]) {
+    if (isFloat[curReg]) {
       return;
     } else {
-      isFloat[curInput] = true;
+      isFloat[curReg] = true;
     }
   }
 
-  if (input[curInput].length < screenNumLim) {
-    input[curInput].push(btnValue);
+  // replace zero in register with num > 0, when there's only zero in register
+  if (inputRegister[curReg].length === 1 && (btnValue !== '0' && btnValue !== '.') && inputRegister[curReg][0] === 0) {
+    inputRegister[curReg][0] = btnValue;
+    // Otherwise just push the number to register
+  } else if (inputRegister[curReg].length < screenNumLim) {
+    inputRegister[curReg].push(btnValue);
   }
 
   updateScreen();
+
 }
 const processFuncpadInput = (e) => {
 
     operation = e.currentTarget.dataset['func'];
-    // saves user input into second array now
 
-  if (curInput === 1 && input[1].length > 0) {
+  // operate on inputs when switched to second register
+  if (curReg === 1) {
 
-    let num1 = parseFloat(input[0].join(''));
-    let num2 = parseFloat(input[1].join(''));
+    // make numbers to operate on
+    let num1 = parseFloat(inputRegister[0].join(''));
+    let num2 = parseFloat(inputRegister[1].join(''));
 
-    input[0] = [];
-    input[0].push(operations[operation](num1, num2));
-    curInput = 0;
-    input[1] = [];
+    if (num2 === 0 && operation === 'div') {
+      // do something witty
+    }
+
+    // push result to first register for it be displayed
+    inputRegister[0] = [];
+    inputRegister[0].push(ops[operation](num1, num2));
+
+    // reset for next operation
+    inputRegister[1] = [0];
     isFloat[1] = false;
-
+    operation = undefined;
+    curReg = 0
+    // Display the result
     updateScreen();
   }
-
-    curInput = 1;
-
 }
-const eraseNum = () => {
-  input[curInput].pop();
+const clearEntry = () => {
+  inputRegister[curReg] = [0];
+  isFloat[curReg] = false;
   updateScreen();
 }
 const updateScreen = () => {
-  screen.textContent = input[curInput].join('');
+  screen.textContent = inputRegister[curReg].join('');
 }
 const clearAll = function onACPressResetEverything() {
-    input = [[],[]]
-    curInput = 0;
-    operation = "";
+    inputRegister = [[0],[0]]
+    isFloat = [false, false];
+    curReg = 0;
+    operation = undefined;
     calcMem = 0;
     updateScreen();
 }
@@ -70,7 +81,7 @@ const shrinkRow = function onButtonClickShrinkButtonRow () {
 
 }
 
-operations = {
+ops = {
   'sum' : (num1, num2) => num1 + num2,
   'sub' : (num1, num2) => num1 - num2,
   'div' : (num1, num2) => num1 / num2,
@@ -78,10 +89,18 @@ operations = {
   'perc' : (num1, num2) => num1 * (num2/100),
 }
 
-let input = [[],[]]
+/*
+memOps = {
+  'madd' : ()=>,
+  'msub' : ()=>,
+  'mrc' : ()=>,
+  'mrc' : ()=>,
+}
+*/
 
-// tracks which in
-let curInput = 0;
+let inputRegister = [[0],[0]]
+// tracks which register to display and write in
+let curReg = 0;
 let operation = undefined;
 let isFloat = [false, false];
 
@@ -98,5 +117,10 @@ numPad.forEach(numBtn => numBtn.addEventListener('click', processNumpadInput))
 const acBtn = document.querySelector("#ac")
 acBtn.addEventListener('click', clearAll);
 
+const ceBtn = document.querySelector('.ce-btn');
+ceBtn.addEventListener('click', clearEntry);
+
 const funcPad = document.querySelectorAll('.func-btn');
 funcPad.forEach(funcBtn => funcBtn.addEventListener('click', processFuncpadInput));
+
+updateScreen();
